37c58c66e43c8f32a8601a3042c9cf198b29b45d
Егор Пилипенко <egor.pilipenko.1998@gmail.com>
2018-02-22 18:36:34 +0300

    friday

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pust_mass;
+  let zakr_brk=0;
+  let otkr_brk=0;
+    for(let i=0;i<bracketsConfig.length;i++){
+
+      for(let j=0; j<str.length; j++){
+      if(str[j]==bracketsConfig[i][0])
+      {
+          otkr_brk++;;
+      }
+      else
+          zakr_brk++;
+      }
+
+  }
+  if(otkr_brk==zakr_brk)
+      return true;
+    else return false;
 }
