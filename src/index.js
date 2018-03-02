03a3872c3d7e71290813f02643999ae0b3bf8eab
Егор Пилипенко <egor.pilipenko.1998@gmail.com>
2018-03-02 03:07:54 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  let pust_mass;
-  let zakr_brk=0;
-  let otkr_brk=0;
-    for(let i=0;i<bracketsConfig.length;i++){
+module.exports =function check(str, bracketsConfig) {
+    str = str.split('');
+    for (let i = 0; i < bracketsConfig.length; i++) {
 
-      for(let j=0; j<str.length; j++){
-      if(str[str.length-1]==bracketsConfig[i][0]){
+        for (let j = 0; j < str.length; j++) {
+
+            if (bracketsConfig[i][0] == str[j] && str[j + 1] == bracketsConfig[i][1]) {
+                str.splice(j, 2);
+                i=0;
+                j = -1;
+            }
+        }
+    }
+    if (str.length != 0) {
         return false;
-      }
-        if(str[j]==bracketsConfig[i][0])
-      {
-          otkr_brk++;;
-      }
-      else
-          zakr_brk++;
-      }
+    }
+    else {
+        return true;
+    }
+};
 
-
-  }
-  if(otkr_brk==zakr_brk)
-      return true;
-    else return false;
-}
